bc8ba1b452674576e47960e719794758c4505a2f
Ajay <ajayb.tech29@gmail.com>
2026-03-15 00:57:29 +0530

    Add basic arithmetic operations in arithmetical.py

---
+++ DailyPython/arithmetical.py
@@ -0,0 +1,15 @@
+#TYPES OF ARTHEMETICAL OPERATORS IN PYTHON
+a=50
+b=10
+#additional
+print(a+b)
+#substraction
+print(a-b)
+#multiplication
+print(a*b)
+#division
+print(a/b)
+#modular division
+print(a%b)
+#a to the power b(a^b)
+print(a**b)
