9c1a157188e8cbc5d1a246370d8936ed2ab885fa
Ajay <ajayb.tech29@gmail.com>
2026-03-15 15:04:52 +0530

    Create String_Scilicing.py

---
+++ DailyPython/String_Scilicing.py
@@ -0,0 +1,10 @@
+#STRING SCILICING
+name="mynameisajay"
+print(name[0:2])
+print(name[2:6])
+print(name[6:8])
+print(name[8:12])
+#from starting to ending without index
+print(name[:])
+#we can also use len(str)
+print(name[:len(name)])
